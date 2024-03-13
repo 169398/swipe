a450aa93a3cd48efd3b509a5f237006c071cb793
169398 <kulubiidris@gmail.com>
2024-03-13 10:08:52 +0300

    topbar changes

--- swipe/app/(root)/layout.tsx
+++ swipe/app/(root)/layout.tsx
@@ -13,7 +13,7 @@ import Topbar from "@/components/shared/Topbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "swipe",
+  title: "Swipe",
   description: "A Strathmore univesity connect application",
 };
 
